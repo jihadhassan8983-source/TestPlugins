d52a469573351e99b29a8da9b556dfbbeea4b38f
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-08-29 18:38:05 +0600

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -7,6 +7,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class ExamplePlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(MoviezWapProvider())
+        registerMainAPI(AnimeThProvider())
     }
 }
