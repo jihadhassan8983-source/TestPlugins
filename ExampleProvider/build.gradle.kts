db9ef7a05737faafcc929a83433c1fa6408c6c9a
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-08-29 17:14:10 +0600

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -3,31 +3,16 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
+    description = "MoviezWap Telugu Tamil Hindi movies"
+    authors = listOf("jihadhassan8983-source")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
     requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    language = "te"
+    iconUrl = "https://www.moviezwap.golf/images/favicon.ico"
 }
 
 android {
