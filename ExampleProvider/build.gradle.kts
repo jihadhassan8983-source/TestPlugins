fa4161c83b97c00d49b8ab200602d84ab0c9acf5
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-08-29 18:39:29 +0600

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,23 +1,9 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
-
-version = 1
-
 cloudstream {
-    description = "MoviezWap Telugu Tamil Hindi movies"
+    description = "Anime-TH Thai sub and dub anime"
     authors = listOf("jihadhassan8983-source")
     status = 1
-    tvTypes = listOf("Movie", "TvSeries")
+    tvTypes = listOf("Anime", "AnimeMovie")
     requiresResources = true
-    language = "te"
-    iconUrl = "https://www.moviezwap.golf/images/favicon.ico"
-}
-
-android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
-    }
+    language = "th"
+    iconUrl = "https://anime-th.com/assets/image/screen.jpg"
 }
