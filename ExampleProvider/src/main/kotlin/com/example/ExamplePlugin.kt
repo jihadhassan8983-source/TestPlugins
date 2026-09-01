d093b523d99766118f5a42f611d3e5e10ac7e5b2
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-09-01 16:49:46 +0600

    Refactor load method in ExamplePlugin
    Removed commented lines for clarity and streamlined the load method.

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -6,13 +6,9 @@ import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class ExamplePlugin : Plugin() {
-    override fun load(context: Context) {
-        // আপনার আগের ৩টি এক্সটেনশন 
-        registerMainAPI(AnimeThProvider())
-        registerMainAPI(AnimeHDProvider())
-        registerMainAPI(AnimeSaltProvider())
-        
-        // আপনার নতুন এক্সটেনশন
-        registerMainAPI(AnimeDriveProvider())
-    }
+override fun load(context: Context) {
+registerMainAPI(AnimeThProvider())
+registerMainAPI(AnimeHDProvider())
+registerMainAPI(AnimeSaltProvider())
+}
 }
