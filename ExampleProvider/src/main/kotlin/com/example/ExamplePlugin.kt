12b6c34587ddc6cc2715a8869895a5d57e471181
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-09-01 14:52:07 +0600

    Refactor ExamplePlugin to use BasePlugin
    Refactor ExamplePlugin to extend BasePlugin and remove Context parameter from load method.

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,14 +1,20 @@
 package com.example
 
-import android.content.Context
+// import android.content.Context (পুরনো API তে এটা ছিল, এখন আর লাগবে না)
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.BasePlugin // Plugin এর বদলে BasePlugin
 
 @CloudstreamPlugin
-class ExamplePlugin : Plugin() {
-override fun load(context: Context) {
-registerMainAPI(AnimeThProvider())
-registerMainAPI(AnimeHDProvider())
-registerMainAPI(AnimeSaltProvider())
-}
+class ExamplePlugin : BasePlugin() {
+    
+    // load() এর ভেতরে এখন আর context: Context থাকে না
+    override fun load() {
+        // আপনার আগের ৩টি এক্সটেনশন 
+        registerMainAPI(AnimeThProvider())
+        registerMainAPI(AnimeHDProvider())
+        registerMainAPI(AnimeSaltProvider())
+        
+        // আপনার নতুন এক্সটেনশন 
+        registerMainAPI(AnimeDriveProvider())
+    }
 }
