2eec4b4708d508a3fddf909d741add4174c2beb8
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-09-01 17:10:30 +0600

    Refactor ExamplePlugin and add AnimeDriveProvider
    Refactor ExamplePlugin to extend BasePlugin and add AnimeDriveProvider.

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,14 +1,18 @@
 package com.example
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
-class ExamplePlugin : Plugin() {
-override fun load(context: Context) {
-registerMainAPI(AnimeThProvider())
-registerMainAPI(AnimeHDProvider())
-registerMainAPI(AnimeSaltProvider())
-}
+class ExamplePlugin : BasePlugin() {
+    override fun load() {
+        // তোমার আগের ৩টা (অপরিবর্তিত)
+        registerMainAPI(AnimeThProvider())
+        registerMainAPI(AnimeHDProvider())
+        registerMainAPI(AnimeSaltProvider())
+
+        // নতুন AnimeDrive
+        registerMainAPI(AnimeDriveProvider())
+    }
 }
