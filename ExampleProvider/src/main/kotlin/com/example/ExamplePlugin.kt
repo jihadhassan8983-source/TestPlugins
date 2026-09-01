28d73c7a2c32b2616e7199730280e103a59e7a18
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-09-01 17:20:40 +0600

    Add AnimeDriveProvider to ExamplePlugin

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,18 +1,14 @@
 package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class ExamplePlugin : BasePlugin() {
     override fun load() {
-        // তোমার আগের ৩টা (অপরিবর্তিত)
         registerMainAPI(AnimeThProvider())
         registerMainAPI(AnimeHDProvider())
         registerMainAPI(AnimeSaltProvider())
-
-        // নতুন AnimeDrive
         registerMainAPI(AnimeDriveProvider())
     }
 }
