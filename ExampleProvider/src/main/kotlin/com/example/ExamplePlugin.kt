f135d447fb1094ed464c6635ea5fdfbc31a3bc64
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-09-01 16:27:14 +0600

    Refactor ExamplePlugin.kt by cleaning up comments
    Removed outdated comments and updated import statement.

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,20 +1,14 @@
 package com.example
 
-// import android.content.Context (পুরনো API তে এটা ছিল, এখন আর লাগবে না)
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin // Plugin এর বদলে BasePlugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class ExamplePlugin : BasePlugin() {
-    
-    // load() এর ভেতরে এখন আর context: Context থাকে না
     override fun load() {
-        // আপনার আগের ৩টি এক্সটেনশন 
         registerMainAPI(AnimeThProvider())
         registerMainAPI(AnimeHDProvider())
         registerMainAPI(AnimeSaltProvider())
-        
-        // আপনার নতুন এক্সটেনশন 
         registerMainAPI(AnimeDriveProvider())
     }
 }
