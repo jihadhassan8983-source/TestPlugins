49c893fda82807f463affa6023d02f54fcf731ca
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-09-02 18:30:47 +0600

    Enhance settings.gradle.kts for local gradle plugin
    Added support for local Cloudstream gradle plugin and updated project inclusion logic.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,14 @@
 rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included.
-// All new projects should get automatically included unless specified in the "disabled" variable.
+// Use local Cloudstream gradle plugin (JitPack is broken for this artifact)
+val csGradle = File(rootDir, "../cs-gradle")
+if (csGradle.exists()) {
+    includeBuild(csGradle) {
+        dependencySubstitution {
+            substitute(module("com.github.recloudstream:gradle")).using(project(":"))
+        }
+    }
+}
 
 val disabled = listOf<String>()
 
@@ -14,6 +21,3 @@ File(rootDir, ".").eachDir { dir ->
 fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
