d2931aedde378c5fbebd3310df2c5b53868956c7
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-09-04 09:35:12 +0600

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -11,6 +11,5 @@ class ExamplePlugin : BasePlugin() {
         registerMainAPI(AnimeDriveProvider())
         registerMainAPI(AnimeKaiProvider())
         registerMainAPI(CineFreakProvider())
-        // MovieLinkBD বাদ — ডিলিট করেছ
     }
 }
