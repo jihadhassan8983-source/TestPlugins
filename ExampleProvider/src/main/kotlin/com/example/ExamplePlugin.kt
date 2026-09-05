9ddb28938fd7ff6f0751a7d01ba2f1f9aac446ec
jihadhassan8983-source <jihadhassan8983@gmail.com>
2026-09-05 11:15:40 +0600

    Update ExamplePlugin to remove specific providers
    Removed AnimeThProvider and AnimeKaiProvider from registration.

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -6,10 +6,8 @@ import com.lagradost.cloudstream3.plugins.BasePlugin
 @CloudstreamPlugin
 class ExamplePlugin : BasePlugin() {
     override fun load() {
-        registerMainAPI(AnimeThProvider())
         registerMainAPI(AnimeHDProvider())
         registerMainAPI(AnimeDriveProvider())
-        registerMainAPI(AnimeKaiProvider())
         registerMainAPI(CineFreakProvider())
         registerMainAPI(FlixmetProvider())
         registerMainAPI(HiAnimeProvider())
